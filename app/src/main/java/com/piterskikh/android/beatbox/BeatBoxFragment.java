6e98585db1720f3ff4d1d89d71130b2808758a21
Exubit <sergei.piterskih@exubit.com>
2018-07-01 19:59:09 +0800

    Initial commit

---
+++ app/src/main/java/com/piterskikh/android/beatbox/BeatBoxFragment.java
@@ -0,0 +1,10 @@
+package com.piterskikh.android.beatbox;
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+
+    public static  BeatBoxFragment getNewInstance(){
+        return new BeatBoxFragment();
+    }
+}
